7503972403531efedaf42e8734115455852f62f2
EvanYY <evan_yangyang@leadscloud.com>
2020-07-17 12:01:15 +0800

    init

---
+++ src/pages/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section style="padding: 20px;">
+    <h5>Vue App</h5>
+    <div style="color: red;">
+      <p>
+        子应用修改，基座会加载么(gggggg)
+      </p>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "Home"
+}
+</script>
