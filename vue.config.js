d74d264c6214ca48bdb6af20cb26e7b5dcc1bf1b
EvanYY <evan_yangyang@leadscloud.com>
2020-07-27 16:58:21 +0800

    引入资源路径修改

--- vue.config.js
+++ vue.config.js
@@ -21,6 +21,7 @@ module.exports = {
       }
     },
     output: {
+      publicPath: '/micro-app1/',
       // 微应用的包名，这里与主应用中注册的微应用名称一致
       library: "VueMicroApp",
       // 将你的 library 暴露为所有的模块定义下都可运行的方式
